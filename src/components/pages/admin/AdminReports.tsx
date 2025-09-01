720e36a577d68d2c8cfe53f050d7c84c45d21ac8
Alexander Tzoc <jealtzo26@gmail.com>
2025-09-01 03:24:56 -0600

    Jobs reports

--- src/components/pages/admin/AdminReports.tsx
+++ src/components/pages/admin/AdminReports.tsx
@@ -1,25 +1,44 @@
 import Button from "../../atoms/Button";
 import AdminLayout from "../../templates/AdminLayout";
+import {useState} from "react";
+import MaintenanceJobReports from "../../organisms/MaintenanceJobReports.tsx";
+
+type Report = {
+    name: string;
+    action: () => void;
+}
 
 export default function AdminReports() {
-  const reports = [
-    "Ingresos por semana",
-    "Mantenimientos preventivos del mes",
-    "Uso de repuestos por marca",
-    "Historial de trabajos por cliente",
+ const [showJobModal, setShowJobModal] = useState(false);
+
+
+ const onJobsReport = () => {
+     setShowJobModal(true);
+ }
+
+  const reports: Report[] = [
+      {name: "Reportes de trabajos", action: onJobsReport}
+    // "Ingresos por semana",
+    // "Mantenimientos preventivos del mes",
+    // "Uso de repuestos por marca",
+    // "Historial de trabajos por cliente",
   ];
 
   return (
     <AdminLayout>
       <h2 className="text-2xl font-bold mb-4">Reportes</h2>
       <div className="bg-white p-4 rounded shadow space-y-2">
-        {reports.map((r, i) => (
-          <div key={i} className="flex justify-between items-center p-2 border-b">
-            <span>{r}</span>
-            <Button className="bg-blue-600 hover:bg-blue-700 text-white">Exportar</Button>
+        {reports.map((r) => (
+          <div key={r.name} className="flex justify-between items-center p-2 border-b">
+            <span>{r.name}</span>
+            <Button className="bg-blue-600 hover:bg-blue-700 text-white" onClick={r.action}>Exportar</Button>
           </div>
         ))}
       </div>
+
+        {showJobModal ? <MaintenanceJobReports onClose={() => {
+            setShowJobModal(false)
+        }} /> : null}
     </AdminLayout>
   );
 }
